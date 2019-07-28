b7fa5f4c36c96f1581af747a614ea2a36945ece6
Sam Selfridge <Sam.selfridge@gmail.com>
2019-07-28 16:55:24 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+// const express = require("express");
+// const app = express();
+
+const config = require("./config/site_keys");
+const twilio = require("twilio")(config.accountSid, config.authToken);
+const request = require("request");
+
+function sendMessage() {
+  twilio.messages.create(
+    {
+      to: config.mynumber,
+      from: config.number,
+      body: `Site Down`
+    },
+    (err, message) => {
+      if (err) {
+        console.log("Twilio Error");
+        console.log(err);
+        res.status(444).json("SMS error");
+      }
+      console.log(message.sid);
+    }
+  );
+}
+
+function pingServer() {
+  return new Promise((resolve, reject) => {
+    request("http://www.mapper.bike", function(error, response, body) {
+      if (error || response.statusCode !== 200) {
+        sendMessage();
+        reject();
+      } else {
+        resolve();
+      }
+    });
+  });
+}
+
+function main() {
+  pingServer()
+    .then(() => {
+      console.log("All good");
+      setTimeout(main, 10000);
+    })
+    .catch(() => {
+      sendMessage();
+    });
+}
+
+main();
